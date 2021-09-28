2c76f99f7a54135ebc63d4db46a62eb80bac9308
Yoann Bureau <ybureau@gmail.com>
2021-09-28 09:27:08 +0200

    added audio notification, fixed progressbar

--- src/app/components/session/session.component.ts
+++ src/app/components/session/session.component.ts
@@ -11,34 +11,39 @@ export class SessionComponent implements OnInit {
 
   private currentAttendeeIndex = 0;
   private remainingSeconds = 0;
+  private audioNotification = new Audio();
 
   constructor(
     private sessionService: SessionService,
     private _snackBar: MatSnackBar
   ) {
     // TODO : Remove
-    this.sessionService.setSession(["John", "Jane", "Jack", "Bernard"], false, 5);
+    this.sessionService.setSession(["John", "Jane", "Jack", "Bernard"], false, 0.1);
+
+    // Init audio notification
+    this.audioNotification.src = "../../../assets/notification.mp3";
+    this.audioNotification.load();
   }
 
   ngOnInit(): void {
     this.openSnackBar(`Give controls to ${this.currentDriver()}, then press \"Start\".`, "Start");
   }
 
-  progressBarValue = () => 100 - ((this.remainingSeconds*100)/(this.sessionService.intervals*60));
-  formatCyphers = (value:number) => value.toString().length === 1 ? `0${value}` : `${value}`;
+  progressBarValue = () => 100 - ((this.remainingSeconds * 100) / (this.sessionService.intervals * 60));
+  formatCyphers = (value: number) => value.toString().length === 1 ? `0${value}` : `${value}`;
   timerMinutes = () => Math.floor(this.remainingSeconds / 60);
   timerSeconds = () => this.remainingSeconds - this.timerMinutes() * 60;
 
   currentDriver = () => this.sessionService.attendees[this.currentAttendeeIndex];
 
-  currentNavigator = () => this.sessionService.attendees[this.currentAttendeeIndex+1];
+  currentNavigator = () => this.sessionService.attendees[this.currentAttendeeIndex + 1];
 
   openSnackBar(message: string, action: string) {
     const snackBarRef = this._snackBar.open(
       message,
       action,
       {
-        verticalPosition:"top",
+        verticalPosition: "top",
       }
     );
 
@@ -52,10 +57,25 @@ export class SessionComponent implements OnInit {
 
     const interval = setInterval(() => {
       this.remainingSeconds--;
-      if(this.remainingSeconds === 0) {
+      if (this.remainingSeconds === 0) {
         clearInterval(interval);
+        this.setNextTimer();
       }
     }, 1000);
+  }
+
+  setNextTimer = () => {
+    this.currentAttendeeIndex++;
+    if (this.currentAttendeeIndex >= this.sessionService.attendees.length) {
+      this.currentAttendeeIndex = 0;
+    }
+
+    this.openSnackBar(`${this.currentDriver()} becomes the driver, and ${this.currentNavigator()} the navigator.`, "Start");
+    this.playNotificationSound();
+  }
+
+  playNotificationSound() {
+    this.audioNotification.play();
   }
 
 }
